b744198d25c335d24febca35f2441408f86bf614
vdovin <V.Vdovin@isida.by>
2026-06-19 11:36:56 +0300

    в конфиг добавлен kafka bootstrap для MD

--- src/main/java/tgb/cryptoexchange/orders/config/CommonConfig.java
+++ src/main/java/tgb/cryptoexchange/orders/config/CommonConfig.java
@@ -101,7 +101,8 @@ public class CommonConfig {
     @Bean
     @Profile("!kafka-disabled")
     public ConsumerFactory<String, MerchantCallbackEvent> merchantCallbackEventConsumerFactory(
-            @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers, ObjectMapper objectMapper) {
+            @Value("${spring.kafka.merchant-details.merchant-details.bootstrap-servers}") String bootstrapServers,
+            ObjectMapper objectMapper) {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         StringDeserializer keyDeserializer = new StringDeserializer();
@@ -114,7 +115,7 @@ public class CommonConfig {
     @Bean
     @Profile("!kafka-disabled")
     public ConcurrentKafkaListenerContainerFactory<String, MerchantCallbackEvent> merchantCallbackKafkaListenerContainerFactory(
-            @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers,
+            @Value("${spring.kafka.merchant-details.bootstrap-servers}") String bootstrapServers,
             ObjectMapper objectMapper, ErrorHandler errorHandler) {
         ConcurrentKafkaListenerContainerFactory<String, MerchantCallbackEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
